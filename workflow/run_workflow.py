02ea49062ced29e5a0707b9162ac4bc59ddbc68c
Isabelle R.V. <88784791+misabellerv@users.noreply.github.com>
2024-09-24 11:57:20 -0300

    Update run_workflow.py

--- workflow/run_workflow.py
+++ workflow/run_workflow.py
@@ -1,14 +1,12 @@
 import warnings
 import time
+from augmentation.apply_aug import augment_df
 from config import get_config
 from data_preparation.dataset import DataPreparation
-from data_preparation.split_data import data_split
 from preprocessing.pipeline import preprocess_pipeline
 from model.models import ModelSelection
 from model.grid_seach import GSCVTrainer
 from model.param_grid import extract_param_grid
-from model.train import Trainer
-from model.get_hyperparam import get_best_hyperparam
 warnings.filterwarnings('ignore')
 
 def run_workflow(config_path):
@@ -22,23 +20,21 @@ def run_workflow(config_path):
     data_prep = DataPreparation(config)
     X_train, X_test, y_train, y_test = data_prep.load_data()
     
-    # Data augmentation
+   # Data augmentation
+    X_train_aug, y_train_aug = augment_df(X_train, y_train)
     
-    
-
     # Pre Process Data
-    X_proc_train = preprocess_pipeline(X_train, config)
-
-    # Data Split (training/validation)
-    X_train_, y_train_, X_val, y_val = data_split(config=config, dataframe=X_proc_train, target=y_train)
+    X_proc_train = preprocess_pipeline(X_train_aug, config)
+    
+    X_proc_test = preprocess_pipeline(X_test, config)
 
     # Get Models
     model = ModelSelection(config)
 
     svm = model.svm()
-    dt = model.dt()
-    xgb = model.xgb()
     knn = model.knn()
+    xgb = model.xgb()
+    dt = model.dt()
 
     # Extract Grid Parameter
     param_svm, param_dt, param_xgb, param_knn = extract_param_grid(config)
@@ -49,38 +45,15 @@ def run_workflow(config_path):
     trainer_xgb = GSCVTrainer(model=xgb, param_grid=param_xgb, config=config)
     trainer_knn = GSCVTrainer(model=knn, param_grid=param_knn, config=config)
 
-    trainer_svm.fit(X_train_, y_train_)
-    trainer_dt.fit(X_train_, y_train_)
-    trainer_xgb.fit(X_train_, y_train_)
-    trainer_knn.fit(X_train_, y_train_)
-
-    # Predict on Validation Set
-    trainer_svm.predict(X_val,y_val)
-    trainer_dt.predict(X_val,y_val)
-    trainer_xgb.predict(X_val,y_val)
-    trainer_knn.predict(X_val, y_val)
-
-    # train+val Training using Best Hyperparameters
-    svm_hyperparam = get_best_hyperparam('SVC')
-    model_train_val_svm = Trainer(model=svm, params=svm_hyperparam, config=config)
-    model_train_val_svm.fit(X_train, y_train)
+    trainer_svm.fit(X_proc_train, y_train_aug)
+    trainer_knn.fit(X_proc_train, y_train_aug)
+    trainer_xgb.fit(X_proc_train, y_train_aug)
+    trainer_dt.fit(X_proc_train, y_train_aug)
     
-    dt_hyperparam = get_best_hyperparam('DecisionTreeClassifier')
-    model_train_val_dt = Trainer(model=dt, params=dt_hyperparam, config=config)
-    model_train_val_dt.fit(X_train, y_train)
-    
-    xgb_hyperparam = get_best_hyperparam('XGBClassifier')
-    model_train_val_xgb = Trainer(model=xgb, params=xgb_hyperparam, config=config)
-    model_train_val_xgb.fit(X_train, y_train)
-    
-    knn_hyperparam = get_best_hyperparam('KNeighborsClassifier')
-    model_train_val_knn = Trainer(model=knn, params=knn_hyperparam, config=config)
-    model_train_val_knn.fit(X_train, y_train)
-
     # Predict on Test Set
-    model_train_val_svm.predict(X_test, y_test)
-    model_train_val_dt.predict(X_test, y_test)
-    model_train_val_xgb.predict(X_test, y_test)
-    model_train_val_knn.predict(X_test, y_test)
+    trainer_svm.predict(X_proc_test, y_test)
+    trainer_knn.predict(X_proc_test, y_test)
+    trainer_xgb.predict(X_proc_test, y_test)
+    trainer_dt.predict(X_proc_test, y_test)
     
     print(f'Full Workflow Completed in {time.time() - start_time:.2f} Seconds.')
