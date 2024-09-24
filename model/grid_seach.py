f13d0236532d19b70d16a45304632775e5fe3a31
Isabelle R.V. <88784791+misabellerv@users.noreply.github.com>
2024-09-24 12:02:09 -0300

    Update grid_seach.py

--- model/grid_seach.py
+++ model/grid_seach.py
@@ -17,6 +17,7 @@ class GSCVTrainer:
         self.n_jobs = config.Joblib.NumJobs
         self.verbose = config.Training.GridSearchCV.Verbose
         self.grid_search = None
+        self.best_model_path = None  
 
     def fit(self, X_train, y_train):
         model_name = self.model.__class__.__name__
@@ -24,7 +25,6 @@ class GSCVTrainer:
         start_time = time.time()
         
         if self.parallel: 
-        
             self.grid_search = GridSearchCV(
                 estimator=self.model,
                 param_grid=self.param_grid,
@@ -41,7 +41,7 @@ class GSCVTrainer:
                 scoring=self.scoring,
                 verbose=self.verbose
             )
-            
+        
         self.grid_search.fit(X_train, y_train)
         print(f"Model Training Finished in {time.time() - start_time:.2f} Seconds!")
 
@@ -52,8 +52,8 @@ class GSCVTrainer:
         with open(best_params_path, 'w') as json_file:
             json.dump(self.grid_search.best_params_, json_file)
 
-        best_model_path = results_path + '/best_model.pkl'
-        joblib.dump(self.grid_search.best_estimator_, best_model_path)
+        self.best_model_path = f'results/model/{model_name}_best_model.pkl'
+        joblib.dump(self.grid_search.best_estimator_, self.best_model_path)
         
         del X_train, y_train
         gc.collect()
@@ -62,28 +62,34 @@ class GSCVTrainer:
 
     def predict(self, X_test, y_test):
         model_name = self.model.__class__.__name__
-        if self.grid_search is None:
-            raise ValueError("The model has not been trained yet. Call `fit` or `grid_search_fit` method before `predict`.")
+
+        if os.path.exists(self.best_model_path):
+            print(f"Loading best model from {self.best_model_path}...")
+            best_model = joblib.load(self.best_model_path) 
+        else:
+            if self.grid_search is None:
+                raise ValueError("The model has not been trained yet. Train the model first or provide a trained model.")
+            best_model = self.grid_search.best_estimator_  
         
         print("Starting Predictions...")
-        self.predictions = self.grid_search.predict(X_test)
+        self.predictions = best_model.predict(X_test) 
         metrics_df = get_metrics(y_test, self.predictions)
 
         metrics = 'results/metrics'
         os.makedirs(metrics, exist_ok=True)
-        
         metrics_df.to_csv(metrics + f'/{model_name}_metrics.csv')
-        
+
         c_matrix = 'results/plots'
         os.makedirs(c_matrix, exist_ok=True)
-        
-        if hasattr(self.model, 'classes_'):
-            labels = self.model.classes_
+
+        if hasattr(best_model, 'classes_'):
+            labels = best_model.classes_
         else:
-            labels = sorted(set(y_test)) 
+            labels = sorted(set(y_test))
             print(f"Model does not have 'classes_' attribute. Using labels from y_test: {labels}")
+        
         plot_confusion_matrix(y_test, self.predictions, labels, os.path.join(c_matrix, f'{model_name}_val_confusion_matrix.png'))
-            
+
         print("Prediction Results Finished!")
         
         del X_test, y_test
